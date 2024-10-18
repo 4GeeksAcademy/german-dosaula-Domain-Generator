e3a15b71c525597b622ab9ba66229aafbc626e9c
LaHuella <german.dosaula@gmail.com>
2024-10-18 20:11:07 +0000

    Web 1.0

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
+let pronoun = ["the", "our", "my", "your", "their", "a", "some", "his", "her", "its"];
+let adj = ["wild", "dangerous", "brutal", "hostile", "desolate", "violent", "unforgiving"];
+let noun = [
+    "Grub", "Naked", "Hazzy", "Boom", "Doomed", "Oil", 
+    "FullHQ", "Scrappy", "Sulfur", "AK47", "Bandit", "Outpost"
+];
+let extension = [".com", ".net", ".org", ".io", ".gg", ".tv", ".es", ".uk", ".us", ".eu", ".ru", ".it"];
+
 function getRandomElement(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
 function generateDomains() {
     const DomainName = [];
-    const numberOfDomains = 15;
-
+    const numberOfDomains = 15; 
     for (let i = 0; i < numberOfDomains; i++) {
         const p = getRandomElement(pronoun);
         const a = getRandomElement(adj);
         const n = getRandomElement(noun);
         const e = getRandomElement(extension);
+        
         
         DomainName.push(p + a + n + e);
         
@@ -23,9 +31,16 @@ function generateDomains() {
 
     const resultDiv = document.getElementById('result');
     
+
     resultDiv.innerHTML = '';
 
+
     DomainName.forEach(domain => {
-        resultDiv.innerHTML += domain + '<br>';
+        resultDiv.innerHTML += domain + '<br>'; 
     });
 }
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('generateButton').addEventListener('click', generateDomains);
+});
